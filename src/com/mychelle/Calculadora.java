3ab3162b9092ab729a27b687b235858a8559e6fd
mychelle-comput <mychelinha7@hotmail.com>
2022-07-12 23:11:47 -0300

    Adicionei um switch case

--- src/com/mychelle/Calculadora.java
+++ src/com/mychelle/Calculadora.java
@@ -5,24 +5,44 @@ import java.util.Scanner;
 public class Calculadora {
 
     public static void main(String[] args) {
-        System.out.println("Calculadora!!!");
+        System.out.println(">>>>Calculadora<<<<");
+
         double num1, num2;
         double resultado;
 
-        System.out.println("\nDigite o primeiro numero:");
         Scanner ler = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero:");
         num1 = ler.nextDouble();
         System.out.println("Digite o segundo numero:");
         num2 = ler.nextDouble();
-        Calculos calcular = new  Calculos();
-        resultado = calcular.somar(num1,num2);
-        System.out.println("A soma dos números = " + new Double(resultado) .intValue());
-        resultado = calcular.subtrair(num1, num2);
-        System.out.println("A subtração dos números = "+ new Double(resultado) .intValue());
-        resultado = calcular.multiplicar(num1,num2);
-        System.out.println("A multiplicação dos numeros = " + resultado);
-        resultado = calcular.dividir(num1,num2);
-        System.out.println("A divisão dos números = " + resultado);
+        System.out.println("Escolha uma operação:");
+        System.out.println("+Soma\n-Sutração\n*Multiplicação\n/Divisão\n#Finalizar");
+        String op = ler.next();
+        Calculos calcular = new Calculos();
+
+        switch (op) {
+            case "+":
+                resultado = calcular.somar(num1, num2);
+                System.out.println("O resultado da soma hé= " + new Double(resultado).intValue());
+                break;
+            case "-":
+                resultado = calcular.subtrair(num1, num2);
+                System.out.println("O resultado da subtração hé= " + new Double(resultado).intValue());
+                break;
+            case "*":
+                resultado = calcular.multiplicar(num1, num2);
+                System.out.println("O resultado da multiplicação hé= " + resultado);
+                break;
+            case "/":
+                resultado = calcular.dividir(num1, num2);
+                System.out.println("O resultado da divisão hé= " + resultado);
+                break;
+            case "#":
+                System.out.println("Calculadora encerrada!");
+                break;
+            default:
+                System.out.println("Essa não é uma opção valida!!!");
+        }
 
     }
 }
